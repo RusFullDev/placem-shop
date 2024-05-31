1f687e3eb7302e6ad4cf830e98046132850e7945
RusFullDev <urinrust@gmail.com>
2024-05-31 09:51:39 +0500

    footer added

--- src/components/SectionFooter2.vue
+++ src/components/SectionFooter2.vue
@@ -38,29 +38,27 @@ const contact = reactive({
     ],
 });
 
-const socials = reactive({
-    socials: [
-        {
-            title: 'Мы в соцсетях',
-            social: [
-                '/socials/Vector.png',
+const socials = reactive( 
+            [
+            '/socials/vk-brands.png',
                 '/socials/instagram.png',
-                '/socials/vk-brands.png'
+                '/socials/Vector.png',
             ]
-        }
-    ],
-    footerBottom: [
-        {
-            footerLogo: '/socials/Лого.png',
-            date: '© PlaceMik.ru, 2021'
-        }
-    ]
-})
+            )
+    
+    // footerBottom: [
+    //     {
+    //         footerLogo: '/socials/Лого.png',
+    //         date: '© PlaceMik.ru, 2021'
+    //     }
+    // ]
+
 
 </script>
 
 <template>
-    <div class="flex gap-[99px] px-5">
+    <div class="bg-grad">
+    <div class="flex gap-[99px] px-10 py-10 pb-8">
         <div v-for="(section, sectionIndex) in contact" :key="sectionIndex">
             <div v-for="(item, itemIndex) in section" :key="itemIndex">
                 <a href="">{{ item.title }}</a>
@@ -69,6 +67,24 @@ const socials = reactive({
                 </div>
             </div>
         </div>
+        <div>
+            <h3 class="pb-6 text-white">Хотите быть в курсе последних новостей и акций? </h3>
+            <div class="flex gap-3 pb-5">
+                <input type="text" placeholder="konstantinkonstantinov@yandex.ru" class="w-[260px] h-[35px] border border-white text-center">
+            <button class="w-[130px] h-[35px] bg-white">Подписаться</button>
+            </div>
+            <div>
+                <p >Мы в соцсетях</p>
+            <div  class="flex gap-2 items-center pt-5">
+                <img v-for="item,index in socials" :key="index" :src="item" alt="">
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="flex justify-between items-center px-10">
+        <img src="/socials/Лого.png" alt="">
+        <p>© PlaceMik.ru, 2021</p>
+    </div>
     </div>
 </template>
 
